dc88503efdc1303b4d45374bf53224dfd81794be
quocnam201010 <167393291+bitchsmp@users.noreply.github.com>
2025-10-25 08:57:13 +0700

    Init Commit

---
+++ src/main/java/dueris/attributeModifierRemover/AttributeModifierRemover.java
@@ -0,0 +1,17 @@
+package dueris.attributeModifierRemover;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AttributeModifierRemover extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
